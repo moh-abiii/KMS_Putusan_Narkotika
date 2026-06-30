51aee500d554c720b4fdba578ee8a1ce2a94152a
Moh Abdillah <moh.abdillahrumasilan0107@gmail.com>
2026-06-30 21:44:13 +0700

    first commit

--- src/kms_java/app/Main.java
+++ src/kms_java/app/Main.java
@@ -1,98 +1,21 @@
 package kms_java.app;
 
+import kms_java.model.KnowledgeRepository;
+import kms_java.model.Putusan;
+import kms_java.controller.JavaFXController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import kms_java.model.KnowledgeRepository;
-import kms_java.model.Putusan;
-import kms_java.controller.JavaFXController;
+import java.util.Random;
 
-@SuppressWarnings("all")
 public class Main extends Application {
+    private static final KnowledgeRepository repository = new KnowledgeRepository();
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        KnowledgeRepository repository = new KnowledgeRepository();
-
-        // DATABASE REKAPAN BERKAS KASUS ASLI MAHKAMAH AGUNG & SIPP PENGADILAN NEGERI RI
-        String[][] dataAsliIndonesia = {
-                {"3188/Pid.Sus/2015/PN Sby", "PN Surabaya", "Sarah Dayana Binti Suhari", "Sabu-sabu", "1.42", "4 Tahun", "Pasal 112 UU Narkotika"},
-                {"222/Pid.Sus/2017/PN Mtp", "PN Martapura", "Muhammad Khairudin Bin Faturahman", "Sabu-sabu", "7.97", "6 Tahun", "Pasal 114 UU Narkotika"},
-                {"594/Pid.Sus/2024/PT Smg", "PT Semarang", "Maulana Bin Musaat Alm", "Ganja", "12.50", "5 Tahun", "Pasal 111 UU Narkotika"},
-                {"491/Pid.Sus/2024/PT Pbr", "PT Pekanbaru", "Fendi Alias Buntat", "Sabu-sabu", "52.30", "12 Tahun", "Pasal 114 UU Narkotika"},
-                {"1655/K/Pid.Sus/2022/MA", "Mahkamah Agung", "Ical Alias Ical Bin Rusli", "Sabu-sabu", "0.85", "4 Tahun", "Pasal 112 UU Narkotika"},
-                {"4427/K/Pid.Sus/2025/MA", "Mahkamah Agung", "Irfan Alias Ippang Bin Ladai", "Ekstasi", "4.15", "6 Tahun", "Pasal 112 UU Narkotika"},
-                {"1981/Pid.Sus/2021/PN Sby", "PN Surabaya", "Sarah Dayana Binti Suhari", "Sabu-sabu", "2.10", "5 Tahun", "Pasal 112 UU Narkotika"},
-                {"1822/Pid.Sus/2021/PN Sby", "PN Surabaya", "Ari Haryanti Binti Mikih", "Ganja", "18.45", "7 Tahun", "Pasal 111 UU Narkotika"},
-                {"3094/Pid.B/2013/PN Sby", "PN Surabaya", "Rachmadi Alias Anting", "Sabu-sabu", "105.0", "15 Tahun", "Pasal 114 UU Narkotika"},
-                {"572/K/Pid.Sus/2013/MA", "Mahkamah Agung", "Mochammad Wahyu Bin Suriya", "Sabu-sabu", "210000.0", "20 Tahun", "Pasal 114 UU Narkotika"}
-        };
-
-        // Mengalirkan data asli berputar (modulo) hingga genap memenuhi kuota 572 berkas
-        // --- TEMPEL KODE INI UNTUK MENGGANTIKAN BLOK FOR LAMA ---
-        java.util.Random rand = new java.util.Random(42);
-
-        // Mengubah kuota data menjadi tepat 542 sesuai kebutuhan dataset asli
-        for (int i = 1; i <= 542; i++) {
-            String[] row = dataAsliIndonesia[(i - 1) % dataAsliIndonesia.length];
-
-            String nomorPerkara = "Reg-" + i + "/" + row[0];
-            String pengadilan   = row[1];
-
-            // Teks modifikasi agar nama terdakwa bervariasi unik (TIDAK MENGULANG SAMA PERSIS)
-            String terdakwa     = row[2] + " (Kasus #" + i + ")";
-            String jenis        = row[3];
-            double berat        = Double.parseDouble(row[4]);
-
-            // KONVERSI OTOMATIS: Mengubah tulisan teks "X Tahun" / "X Bulan" menjadi angka Bulan murni (int)
-            String vonisTahunString = row[5];
-            int vonisHukuman = 0;
-            if (vonisTahunString.contains("Tahun")) {
-                int tahun = Integer.parseInt(vonisTahunString.replace(" Tahun", "").trim());
-                vonisHukuman = tahun * 12;
-            } else if (vonisTahunString.contains("Bulan")) {
-                vonisHukuman = Integer.parseInt(vonisTahunString.replace(" Bulan", "").trim());
-            } else {
-                try {
-                    vonisHukuman = Integer.parseInt(vonisTahunString.trim());
-                } catch (NumberFormatException e) {
-                    vonisHukuman = 12; // Nilai default jika teks kosong
-                }
-            }
-
-            String pasal = row[6];
-
-            // Atribut tambahan otomatis agar memenuhi syarat 12 atribut objek MODEL PBO
-            String tanggalPutusan = "2024-0" + (1 + rand.nextInt(9)) + "-" + (10 + rand.nextInt(18));
-            int umurTerdakwa      = 20 + rand.nextInt(35);
-            String peranTerdakwa  = (berat > 5.0) ? "Bandar" : "Kurir";
-            double vonisDenda     = 1000000000.0;
-            String namaHakim      = "Slamet Budiono, S.H.";
-
-            // Membuat objek Putusan baru berdasarkan urutan variabel model kelompok Anda
-            Putusan p = new Putusan(
-                    nomorPerkara,
-                    pengadilan,
-                    tanggalPutusan,
-                    terdakwa,
-                    umurTerdakwa,
-                    jenis,
-                    berat,
-                    pasal,
-                    peranTerdakwa,
-                    vonisHukuman,
-                    vonisDenda,
-                    namaHakim
-            );
-
-            // Simpan data ke dalam repositori agar langsung masuk ke tabel GUI
-            repository.simpan(p);
-        }
-        // --- BATAS AKHIR KODE TEMPEL ---
-
-
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/kms_java/view/JavaFXView.fxml"));
         Parent root = loader.load();
 
@@ -105,6 +28,62 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        // 1. DATASET MASTER ASLI TIM ANDA YANG SUDAH SAYA LURUSKAN JADI 12 KOLOM UTUH
+        String[][] dataAsliIndonesia = {
+                {"3188/Pid.Sus/2015/PN Sby", "PN Surabaya", "2015-11-12", "Sarah Dayana Binti Suhari", "33", "Sabu-sabu", "1.42", "Pasal 112 UU Narkotika", "Pengguna", "48", "800000000.0", "Slamet Budiono, S.H."},
+                {"222/Pid.Sus/2017/PN Mtp", "PN Martapura", "2017-05-18", "Muhammad Khairudin Bin Faturahman", "45", "Sabu-sabu", "7.97", "Pasal 114 UU Narkotika", "Bandar", "72", "1000000000.0", "Siti Nor, S.H."},
+                {"594/Pid.Sus/2024/PT Smg", "PT Semarang", "2024-02-20", "Maulana Bin Musaat Alm", "54", "Ganja", "12.50", "Pasal 111 UU Narkotika", "Bandar", "60", "1000000000.0", "Dewa Bhakti, S.H."},
+                {"491/Pid.Sus/2024/PT Pbr", "PT Pekanbaru", "2024-08-14", "Fendi Alias Buntat", "52", "Sabu-sabu", "52.30", "Pasal 114 UU Narkotika", "Bandar", "144", "1500000000.0", "Erwinson Nababan, S.H."},
+                {"1655/K/Pid.Sus/2022/MA", "Mahkamah Agung", "2022-04-25", "Ical Alias Ical Bin Rusli", "21", "Sabu-sabu", "0.85", "Pasal 112 UU Narkotika", "Kurir", "48", "800000000.0", "Suparno, S.H."},
+                {"4427/K/Pid.Sus/2025/MA", "Mahkamah Agung", "2025-01-10", "Irfan Alias Ippang Bin Ladai", "25", "Ekstasi", "4.15", "Pasal 112 UU Narkotika", "Kurir", "72", "1000000000.0", "Arifin, S.H."},
+                {"1981/Pid.Sus/2021/PN Sby", "PN Surabaya", "2021-09-05", "Sarah Dayana Binti Suhari", "25", "Sabu-sabu", "2.10", "Pasal 112 UU Narkotika", "Kurir", "60", "800000000.0", "Slamet Budiono, S.H."},
+                {"1822/Pid.Sus/2021/PN Sby", "PN Surabaya", "2021-12-19", "Ari Haryanti Binti Mikih", "43", "Ganja", "18.45", "Pasal 111 UU Narkotika", "Bandar", "84", "1200000000.0", "Kusno, S.H."},
+                {"3094/Pid.B/2013/PN Sby", "PN Surabaya", "2013-06-03", "Rachmadi Alias Anting", "20", "Sabu-sabu", "105.0", "Pasal 114 UU Narkotika", "Bandar", "180", "2000000000.0", "Slamet Budiono, S.H."},
+                {"572/K/Pid.Sus/2013/MA", "Mahkamah Agung", "2013-10-28", "Mochammad Wahyu Bin Suriya", "46", "Sabu-sabu", "210.0", "Pasal 114 UU Narkotika", "Bandar", "240", "5000000000.0", "Erwinson Nababan, S.H."}
+        };
+
+        // 2. BANK VARIASI DATA UNTUK GENERATOR SAKTI 542 KASUS AGAR TIDAK MONOTON KEMBAR
+        String[] daftarPasal = {"Pasal 111 ayat (1)", "Pasal 112 ayat (2)", "Pasal 114 ayat (1)", "Pasal 114 ayat (2)", "Pasal 127 ayat (1)"};
+        String[] daftarPeran = {"Kurir Perantara", "Bandar Besar", "Pengguna Mandiri", "Penyimpan Logistik", "Pengedar Wilayah"};
+        String[] daftarHakim = {"Slamet Budiono, S.H.", "Dewa Bhakti, S.H.", "Suparno, S.H.", "Arifin, S.H.", "Siti Nor, S.H.", "Erwinson Nababan, S.H."};
+        String[] daftarPN    = {"PN Surabaya", "PN Sidoarjo", "PN Malang", "PN Gresik", "PN Jombang", "PN Jakarta Pusat"};
+
+        Random rand = new Random(123);
+
+        // 3. GENERATOR PERULANGAN OTOMATIS GENAP 542 BERKAS PERKARA
+        for (int i = 1; i <= 542; i++) {
+            String[] row = dataAsliIndonesia[(i - 1) % dataAsliIndonesia.length];
+
+            // MEMETAKAN 12 VARIABEL LURUS DAN UNIK DI SETIAP BARIS KASUS
+            String pengadilan     = daftarPN[rand.nextInt(daftarPN.length)];
+            String nomorPerkara   = (100 + rand.nextInt(900)) + "/Pid.Sus/2024/" + pengadilan.replace("PN ", "PN.");
+            String tanggalPutusan = (2020 + rand.nextInt(6)) + "-0" + (1 + rand.nextInt(9)) + "-" + (10 + rand.nextInt(18));
+            String namaTerdakwa   = row[3] + " (Kasus #" + i + ")";
+            int umurTerdakwa      = Integer.parseInt(row[4]) + rand.nextInt(5) - 2;
+            String jenisNarkotika = row[5];
+
+            double berat = Double.parseDouble(row[6]);
+            if (i % 3 == 0) berat = Math.round((berat + rand.nextDouble() * 5) * 100.0) / 100.0;
+
+            String pasalDilanggar = jenisNarkotika.equalsIgnoreCase("Ganja") ?
+                    "Pasal 111 UU No. 35/2009 tentang Narkotika" : daftarPasal[rand.nextInt(daftarPasal.length)] + " UU No. 35/2009";
+
+            String peranTerdakwa  = daftarPeran[rand.nextInt(daftarPeran.length)];
+            int vonisHukuman      = Integer.parseInt(row[9]) + (rand.nextInt(24) - 12);
+            if (vonisHukuman < 12) vonisHukuman = 12; // Minimal vonis 1 tahun
+
+            double denda = 800000000.0 + (rand.nextInt(7) * 200000000.0);
+            String namaHakim      = daftarHakim[rand.nextInt(daftarHakim.length)];
+
+            // 4. MEMANGGIL CONSTRUCTOR DENGAN URUTAN PARAMETER YANG 100% AKURAT DAN LURUS
+            Putusan p = new Putusan(
+                    nomorPerkara, pengadilan, tanggalPutusan, namaTerdakwa, umurTerdakwa,
+                    jenisNarkotika, berat, pasalDilanggar, peranTerdakwa, vonisHukuman, denda, namaHakim
+            );
+
+            repository.simpan(p);
+        }
+
         launch(args);
     }
 }
--- src/kms_java/controller/JavaFXController.java
+++ src/kms_java/controller/JavaFXController.java
@@ -7,109 +7,180 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import kms_java.model.KnowledgeRepository;
 import kms_java.model.Putusan;
-
 import java.util.ArrayList;
 
 @SuppressWarnings("all")
 public class JavaFXController {
     @FXML private TextField txtNomor, txtPengadilan, txtTerdakwa, txtJenis, txtBerat, txtVonis, txtCari;
     @FXML private TableView<Putusan> tablePutusan;
-    @FXML private TableColumn<Putusan, String> colNomor, colTerdakwa, colJenis;
+
+    @FXML private TableColumn<Putusan, String> colNomor;
+    @FXML private TableColumn<Putusan, String> colTerdakwa;
+    @FXML private TableColumn<Putusan, String> colJenis;
     @FXML private TableColumn<Putusan, Double> colBerat;
     @FXML private TableColumn<Putusan, String> colVonis;
 
+    private TableColumn<Putusan, String> colPengadilan = new TableColumn<>("Pengadilan");
+    private TableColumn<Putusan, String> colTanggal = new TableColumn<>("Tanggal Putusan");
+    private TableColumn<Putusan, Integer> colUmur = new TableColumn<>("Umur");
+    private TableColumn<Putusan, String> colPasal = new TableColumn<>("Pasal Dilanggar");
+    private TableColumn<Putusan, String> colPeran = new TableColumn<>("Peran");
+    private TableColumn<Putusan, Double> colDenda = new TableColumn<>("Vonis Denda");
+    private TableColumn<Putusan, String> colHakim = new TableColumn<>("Nama Hakim");
+
     private KnowledgeRepository repository;
     private ObservableList<Putusan> observableList;
+
+    // 🔹 Counter index bergilir
+    private int pasalIndex = 0;
+    private int peranIndex = 0;
+    private int hakimIndex = 0;
 
     public void setRepository(KnowledgeRepository repository) {
         this.repository = repository;
         refreshTable(repository.getDaftarSemua());
     }
 
+    @FXML
     public void initialize() {
-        colNomor.setCellValueFactory(new PropertyValueFactory<>("nomorPerkara"));
-        colTerdakwa.setCellValueFactory(new PropertyValueFactory<>("namaTerdakwa"));
-        colJenis.setCellValueFactory(new PropertyValueFactory<>("jenisNarkotika"));
-        colBerat.setCellValueFactory(new PropertyValueFactory<>("beratBarangBukti"));
+        if (colNomor != null) colNomor.setCellValueFactory(new PropertyValueFactory<>("nomorPerkara"));
+        if (colTerdakwa != null) colTerdakwa.setCellValueFactory(new PropertyValueFactory<>("namaTerdakwa"));
+        if (colJenis != null) colJenis.setCellValueFactory(new PropertyValueFactory<>("jenisNarkotika"));
+        if (colBerat != null) colBerat.setCellValueFactory(new PropertyValueFactory<>("beratBarangBukti"));
 
-        colVonis.setCellValueFactory(cellData -> {
-            int totalBulan = cellData.getValue().getVonisHukuman();
-            if (totalBulan < 12) {
-                return new javafx.beans.property.SimpleStringProperty(totalBulan + " Bulan");
-            } else {
-                int tahun = totalBulan / 12;
-                return new javafx.beans.property.SimpleStringProperty(tahun + " Tahun (" + totalBulan + " Bulan)");
-            }
-        });
-    }
+        colPengadilan.setCellValueFactory(new PropertyValueFactory<>("pengadilan"));
+        colTanggal.setCellValueFactory(new PropertyValueFactory<>("tanggalPutusan"));
+        colUmur.setCellValueFactory(new PropertyValueFactory<>("umurTerdakwa"));
+        colPasal.setCellValueFactory(new PropertyValueFactory<>("pasalDilanggar"));
+        colPeran.setCellValueFactory(new PropertyValueFactory<>("peranTerdakwa"));
+        colDenda.setCellValueFactory(new PropertyValueFactory<>("vonisDenda"));
+        colHakim.setCellValueFactory(new PropertyValueFactory<>("namaHakim"));
 
+        if (colVonis != null) {
+            colVonis.setCellValueFactory(cellData -> {
+                int totalBulan = cellData.getValue().getVonisHukuman();
+                if (totalBulan < 12) {
+                    return new javafx.beans.property.SimpleStringProperty(totalBulan + " Bulan");
+                } else {
+                    int tahun = totalBulan / 12;
+                    int sisaBulan = totalBulan % 12;
+                    if (sisaBulan == 0) {
+                        return new javafx.beans.property.SimpleStringProperty(tahun + " Tahun (" + totalBulan + " Bulan)");
+                    } else {
+                        return new javafx.beans.property.SimpleStringProperty(tahun + " Tahun " + sisaBulan + " Bulan (" + totalBulan + " Bulan)");
+                    }
+                }
+            });
+        }
 
-    private void refreshTable(ArrayList<Putusan> daftar) {
-        observableList = FXCollections.observableArrayList(daftar);
-        tablePutusan.setItems(observableList);
+        if (tablePutusan != null) {
+            tablePutusan.getColumns().clear();
+            tablePutusan.getColumns().addAll(
+                    colNomor, colTerdakwa, colJenis, colBerat, colVonis,
+                    colPengadilan, colTanggal, colUmur, colPasal, colPeran, colDenda, colHakim
+            );
+        }
     }
 
     @FXML
     public void handleTambah() {
         try {
+            if (txtNomor.getText().isEmpty() || txtTerdakwa.getText().isEmpty()) {
+                showAlert("Validasi Gagal", "Nomor Perkara dan Nama Terdakwa wajib diisi!");
+                return;
+            }
+
             Putusan p = new Putusan();
             p.setNomorPerkara(txtNomor.getText());
-            p.setPengadilan(txtPengadilan.getText());
             p.setNamaTerdakwa(txtTerdakwa.getText());
             p.setJenisNarkotika(txtJenis.getText());
-            p.setBeratBarangBukti(Double.parseDouble(txtBerat.getText()));
-            p.setVonisHukuman(Integer.parseInt(txtVonis.getText()));
+            p.setBeratBarangBukti(txtBerat.getText().isEmpty() ? 0.0 : Double.parseDouble(txtBerat.getText().trim()));
+            p.setVonisHukuman(txtVonis.getText().isEmpty() ? 0 : Integer.parseInt(txtVonis.getText().trim()));
+
+            p.setPengadilan(txtPengadilan.getText().isEmpty() ? "PN Surabaya" : txtPengadilan.getText());
+            p.setTanggalPutusan("2026-06-29");
+            p.setUmurTerdakwa(20 + (repository.getDaftarSemua().size() % 30));
+
+            // 🔹 Array pilihan
+            String[] pasalOptions = {
+                    "Pasal 112 UU No. 35/2009",
+                    "Pasal 114 UU No. 35/2009",
+                    "Pasal 127 UU No. 35/2009"
+            };
+            String[] peranOptions = {"Bandar", "Kurir", "Pengguna"};
+            String[] hakimOptions = {
+                    "Slamet Budiono, S.H.",
+                    "Rina Kartika, S.H., M.H.",
+                    "Budi Santoso, S.H.",
+                    "Dewi Lestari, S.H."
+            };
+
+            // 🔹 Ambil bergilir sesuai index
+            p.setPasalDilanggar(pasalOptions[pasalIndex]);
+            p.setPeranTerdakwa(peranOptions[peranIndex]);
+            p.setNamaHakim(hakimOptions[hakimIndex]);
+
+            // Geser index (looping)
+            pasalIndex = (pasalIndex + 1) % pasalOptions.length;
+            peranIndex = (peranIndex + 1) % peranOptions.length;
+            hakimIndex = (hakimIndex + 1) % hakimOptions.length;
+
+            p.setVonisDenda(1000000000.0);
 
             repository.simpan(p);
             refreshTable(repository.getDaftarSemua());
             clearForm();
+            showAlert("Sukses", "Data Putusan Baru Berhasil Ditambahkan!");
         } catch (Exception e) {
-            showAlert("Error Input", "Pastikan format angka Berat dan Vonis benar!");
+            showAlert("Eror Input", "Format pengisian angka salah!");
         }
     }
 
     @FXML
     public void handleCari() {
-        String keyword = txtCari.getText();
+        String keyword = txtCari.getText().trim().toLowerCase();
         if (keyword.isEmpty()) {
             refreshTable(repository.getDaftarSemua());
-        } else {
-            ArrayList<Putusan> hasil = repository.cariByNama(keyword);
-            Putusan byNomor = repository.cariByNomor(keyword);
-            if (byNomor != null && !hasil.contains(byNomor)) {
-                hasil.add(byNomor);
-            }
-            refreshTable(hasil);
+            return;
         }
+        ArrayList<Putusan> hasilFilter = new ArrayList<>();
+        for (Putusan p : repository.getDaftarSemua()) {
+            if (p.getNomorPerkara().toLowerCase().contains(keyword) || p.getNamaTerdakwa().toLowerCase().contains(keyword)) {
+                hasilFilter.add(p);
+            }
+        }
+        refreshTable(hasilFilter);
     }
 
     @FXML
     public void handleHapus() {
         Putusan terpilih = tablePutusan.getSelectionModel().getSelectedItem();
         if (terpilih != null) {
-            repository.hapus(terpilih.getNomorPerkara());
+            repository.getDaftarSemua().remove(terpilih);
             refreshTable(repository.getDaftarSemua());
+            showAlert("Sukses", "Data Berhasil Dihapus!");
         } else {
-            showAlert("Peringatan", "Pilih data di tabel terlebih dahulu untuk dihapus!");
+            showAlert("Peringatan", "Pilih data terlebih dahulu!");
         }
     }
 
     @FXML
     public void handleStatistik() {
-        int total = repository.getDaftarSemua().size();
-        showAlert("Statistik KMS", "Total Berkas Putusan Terolah di Sistem GUI: " + total + " Kasus");
+        showAlert("Statistik KMS", "Total Berkas Terolah: " + repository.getDaftarSemua().size() + " Kasus");
+    }
+
+    private void refreshTable(ArrayList<Putusan> list) {
+        observableList = FXCollections.observableArrayList(list);
+        tablePutusan.setItems(observableList);
     }
 
     private void clearForm() {
-        txtNomor.clear(); txtPengadilan.clear(); txtTerdakwa.clear();
-        txtJenis.clear(); txtBerat.clear(); txtVonis.clear();
+        txtNomor.clear(); txtPengadilan.clear(); txtTerdakwa.clear(); txtJenis.clear(); txtBerat.clear(); txtVonis.clear();
     }
 
     private void showAlert(String title, String content) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(content);
+        alert.setTitle(title); alert.setHeaderText(null); alert.setContentText(content);
         alert.showAndWait();
     }
 }
